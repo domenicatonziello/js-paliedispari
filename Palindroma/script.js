4b622c393cd45aa30e5ca1c5fff5f95fd0b7cd32
domenicatonziello <domenica.tonziello10@gmail.com>
2022-12-15 16:01:44 +0100

    first commit

---
+++ Palindroma/script.js
@@ -0,0 +1,43 @@
+/*
+Chiedere all’utente di inserire una parola tramite un form
+Creare una funzione per capire se la parola inserita è palindroma
+*/
+
+//* CREO FUNZIONE PER CAPIRE SE LA PAROLA INSERITA E' PALINDROMA:
+function isPalindroma(text){
+    
+    // inverto ordine delle lettere
+    let textReverse = '';
+    for(let i = text.length - 1; i >= 0 ; i-- ){
+        textReverse += text[i];
+    }
+    
+    // Creo variabile booleana
+    let isPalindroma = false;
+    if(textReverse === text){
+        isPalindroma = true;
+    }
+    
+    // restituisco valore
+    return isPalindroma;  
+}
+/*
+const userWord = isPalindroma('osso'); 
+console.log(userWord);
+*/
+
+//* Prendo elementi dal DOM
+const form = document.getElementById('my-form');
+const userText = document.getElementById('text');
+const button = document.getElementById('btn');
+
+//* Creo EVEN LISTENER AL FORM
+form.addEventListener('submit', function(event){
+    // blocco evoluzione form
+    event.preventDefault();
+    // prendo value dell'input
+    const userWord = userText.value.trim().toLowerCase();
+    // assegno funzione alla parola
+    const wordPalindroma = isPalindroma(userWord);
+    console.log(wordPalindroma);
+});
